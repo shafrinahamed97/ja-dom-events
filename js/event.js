ceabc99cf71abd8eff1efebe81f668a2e960785e
shafrinahamed97 <shafrinahamedjob@gmail.com>
2022-08-19 14:09:16 +0600

    added Option4 final

--- js/event.js
+++ js/event.js
@@ -23,3 +23,21 @@ function makeRed()
     {
         document.body.style.backgroundColor = 'purple'
     }
+
+    // Option:4
+    const pinkButton= document.getElementById('make-pink');
+      pinkButton.addEventListener('click', makePink )
+      function makePink(){
+        document.body.style.backgroundColor = 'pink';
+      }
+
+      // Option:4 Other
+      const makeGreenButton = document.getElementById('make-green');
+     makeGreenButton.addEventListener('click', function makeGreen() {
+        document.body.style.backgroundColor = 'green';
+     })
+
+     // Optio:4 Final
+     document.getElementById('make-goldenrod').addEventListener('click',function(){
+        document.body.style.backgroundColor= ' goldenrod';
+     })
